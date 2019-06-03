d125a55f0042b3d557f8b95e42890d9bb962f33e
majing <majing@majing.allinfinance.com>
2019-06-03 10:21:48 +0800

    hahahhahah

---
+++ NioTest/src/com/read/netty/TimeServer.java
@@ -0,0 +1,80 @@
+package com.read.netty;
+
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class TimeServer {
+	public void bind(int port) {
+		NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+		NioEventLoopGroup workGroup = new NioEventLoopGroup();
+		ServerBootstrap b = new ServerBootstrap();
+		b.group(bossGroup, workGroup)
+		.channel(NioServerSocketChannel.class)
+		.option(ChannelOption.SO_BACKLOG, 1024)
+		.childHandler(new ChildChannelHandler());
+		try {
+			ChannelFuture f = b.bind(port).sync();
+			f.channel().closeFuture().sync();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			bossGroup.shutdownGracefully();
+			workGroup.shutdownGracefully();
+		}
+	}
+	
+	public static void main(String[] args) {
+		new TimeServer().bind(14444);
+	}
+}
+class ChildChannelHandler extends ChannelInitializer<SocketChannel>{
+
+	@Override
+	protected void initChannel(SocketChannel arg0) throws Exception {
+		ChannelPipeline pipeline = arg0.pipeline();
+		pipeline.addLast(new TimeServerHandler());
+	}
+	
+}
+class TimeServerHandler extends ChannelInboundHandlerAdapter{
+	@Override
+	public void channelActive(ChannelHandlerContext ctx) throws Exception {
+		ByteBuf copiedBuffer = Unpooled.copiedBuffer("connect success!!".getBytes());
+		ctx.writeAndFlush(copiedBuffer);
+	}
+
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+		ByteBuf buf=(ByteBuf)msg;
+		byte [] bytes=new byte[buf.readableBytes()];
+		buf.readBytes(bytes);
+		String body = new String(bytes,"utf-8");
+		System.out.println(body);
+		String reasult="majing".equalsIgnoreCase(body)?"yes!!!":"no!!!";
+		ByteBuf copiedBuffer = Unpooled.copiedBuffer(reasult.getBytes());
+		ctx.writeAndFlush(copiedBuffer);
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+		System.out.println("连接异常！");
+		ctx.close();
+	}
+
+	
+
+	
+	
+}
+

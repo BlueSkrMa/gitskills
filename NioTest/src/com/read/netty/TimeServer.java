5740bec4b31a283f43308d42092c797a2dad3ad4
blueSkrMa <842670735@qq.com>
2019-06-03 11:47:02 +0800

    哈哈哈

--- NioTest/src/com/read/netty/TimeServer.java
+++ NioTest/src/com/read/netty/TimeServer.java
@@ -1,6 +1,5 @@
 package com.read.netty;
 
-
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
@@ -19,35 +18,35 @@ public class TimeServer {
 		NioEventLoopGroup bossGroup = new NioEventLoopGroup();
 		NioEventLoopGroup workGroup = new NioEventLoopGroup();
 		ServerBootstrap b = new ServerBootstrap();
-		b.group(bossGroup, workGroup)
-		.channel(NioServerSocketChannel.class)
-		.option(ChannelOption.SO_BACKLOG, 1024)
-		.childHandler(new ChildChannelHandler());
+		b.group(bossGroup, workGroup).channel(NioServerSocketChannel.class).option(ChannelOption.SO_BACKLOG, 1024)
+				.childHandler(new ChildChannelHandler());
 		try {
 			ChannelFuture f = b.bind(port).sync();
 			f.channel().closeFuture().sync();
-		}catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
-		}finally {
+		} finally {
 			bossGroup.shutdownGracefully();
 			workGroup.shutdownGracefully();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		new TimeServer().bind(14444);
 	}
 }
-class ChildChannelHandler extends ChannelInitializer<SocketChannel>{
+
+class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
 
 	@Override
 	protected void initChannel(SocketChannel arg0) throws Exception {
 		ChannelPipeline pipeline = arg0.pipeline();
 		pipeline.addLast(new TimeServerHandler());
 	}
-	
+
 }
-class TimeServerHandler extends ChannelInboundHandlerAdapter{
+
+class TimeServerHandler extends ChannelInboundHandlerAdapter {
 	@Override
 	public void channelActive(ChannelHandlerContext ctx) throws Exception {
 		ByteBuf copiedBuffer = Unpooled.copiedBuffer("connect success!!".getBytes());
@@ -56,12 +55,12 @@ class TimeServerHandler extends ChannelInboundHandlerAdapter{
 
 	@Override
 	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-		ByteBuf buf=(ByteBuf)msg;
-		byte [] bytes=new byte[buf.readableBytes()];
+		ByteBuf buf = (ByteBuf) msg;
+		byte[] bytes = new byte[buf.readableBytes()];
 		buf.readBytes(bytes);
-		String body = new String(bytes,"utf-8");
+		String body = new String(bytes, "utf-8");
 		System.out.println(body);
-		String reasult="majing".equalsIgnoreCase(body)?"yes!!!":"no!!!";
+		String reasult = "majing".equalsIgnoreCase(body) ? "yes!!!" : "no!!!";
 		ByteBuf copiedBuffer = Unpooled.copiedBuffer(reasult.getBytes());
 		ctx.writeAndFlush(copiedBuffer);
 	}
@@ -70,11 +69,8 @@ class TimeServerHandler extends ChannelInboundHandlerAdapter{
 	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
 		System.out.println("连接异常！");
 		ctx.close();
+		System.out.println(111);
+		System.out.println("失败了吗！！！！！");
 	}
 
-	
-
-	
-	
 }
-
